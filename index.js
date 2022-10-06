d3de80af20ca46d1ead6b114f3005922a48a3851
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-06 06:18:13 +0530

    added 4

---
+++ index.js
@@ -0,0 +1,5 @@
+const data = require("./v-2-B-students_data.json");
+console.log(data);
+// console.log(data.students[0])
+// console.log(data.students[1])
+console.log(data.students[0][1].friends[0].Name)
