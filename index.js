c0a4d778d73e7e4f85cd562fb7bb3ecfffd20f7e
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-06 11:30:02 +0530

    added 5

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+// how to access json file 
 const data = require("./v-2-B-students_data.json");
-console.log(data);
-// console.log(data.students[0])
-// console.log(data.students[1])
-console.log(data.students[0][1].friends[0].Name)
+// console.log(data);
+
+
+
+
+// how to access json data 
+
+console.log(data.students[0][0].name) // no..1 xample
+console.log(data.students[0][0].languages[2]); // no..2 xample
+console.log(data.students[0][1].name); // no..3 xample
+
+
+
+
+// how to modify json data 
+
+data.students[0][1].name = "sabana" // no..1 xample
+console.log(data.students[0][1].name); 
+
+data.students[0][0].name = "ani vai bd" // no..2 xample
+console.log(data.students[0][0].name); 
+
+data.students[0][0].languages[2] = "urdu" // no..3 xample
+console.log(data.students[0][0].languages[2]); 
+
+
+
+
+
+// how to delete json data 
